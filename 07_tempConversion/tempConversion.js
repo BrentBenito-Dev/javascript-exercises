70df8c51eb0512e81e014c4a25ac62bb2f751649
NeoooDev <124891218+BrentBenito-Dev@users.noreply.github.com>
2025-01-06 15:02:48 +0800

    Add: solutions to 06 and 07 activities

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = (temp - 32 ) / 1.8;
+  temp = temp.toFixed(1);
+  temp = parseFloat(temp);
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = (temp * 1.8) + 32;
+  temp = temp.toFixed(1);
+  temp = parseFloat(temp);
+  return temp;
 };
 
 // Do not edit below this line
